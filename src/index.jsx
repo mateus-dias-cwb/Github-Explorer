a713d2e6c8ff08663ed52f0adf70d413faa99cb1
Mateus Dias <ckateusdias@gmail.com>
2021-09-28 18:44:11 -0300

    Última etapa da preparação

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
 import { render } from 'react-dom'
 import { App } from './App'
 
-render(<h1>Test</h1>, document.getElementByid())
+render(<App />, document.getElementById('root'))
